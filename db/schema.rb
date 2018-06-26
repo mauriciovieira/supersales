f4aab892d89ad20bd9beac42f53d222bccea92fd
Mauricio Vieira <mauricio@mauriciovieira.net>
2018-06-26 21:43:32 +0300

    Create clients

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_26_184151) do
+ActiveRecord::Schema.define(version: 2018_06_26_184311) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "clients", force: :cascade do |t|
+    t.string "name"
+    t.string "company_name"
+    t.string "document"
+    t.string "email"
+    t.string "phone"
+    t.bigint "user_id"
+    t.text "notes"
+    t.integer "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_clients_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -25,4 +39,5 @@ ActiveRecord::Schema.define(version: 2018_06_26_184151) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "clients", "users"
 end
