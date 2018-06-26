824df20f5733e8f6389f226e7f0948101448fc79
Mauricio Vieira <mauricio@mauriciovieira.net>
2018-06-26 21:45:22 +0300

    Create discounts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_26_184443) do
+ActiveRecord::Schema.define(version: 2018_06_26_184505) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,16 @@ ActiveRecord::Schema.define(version: 2018_06_26_184443) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_clients_on_user_id"
+  end
+
+  create_table "discounts", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.integer "value"
+    t.integer "kind"
+    t.integer "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "products", force: :cascade do |t|
