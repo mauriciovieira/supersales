75c67fac40c6ac900de4fac9f6a146dba13f9220
Mauricio Vieira <mauricio@mauriciovieira.net>
2018-06-26 21:52:56 +0300

    Add associations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,4 @@
 class Product < ApplicationRecord
+  enum status: [:active, :inactive]
+  has_many :sales_items
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  enum kind: [:salesperson, :manager]
+  enum status: [:active, :inactive]
+  has_many :comissions
+  has_many :addresses
+  has_many :clients
+  has_many :sales_items
+  has_many :sales
 end
