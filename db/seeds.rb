2657005f015de4ff2d6636a8362dc1278ddbae84
Mauricio Vieira <mauricio@mauriciovieira.net>
2018-06-28 23:29:00 +0300

    Add a new salesperson, emails and passwords to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,6 @@
-User.create name: 'John Doe', status: :active, kind: :salesperson
-User.create name: 'Jane Doe', status: :active, kind: :manager
+User.create name: 'John Smith', status: :active, kind: :salesperson, email: 'john.smith@example.com', password: 'password'
+User.create name: 'John Doe', status: :active, kind: :salesperson, email: 'john.doe@example.com', password: 'password'
+User.create name: 'Jane Doe', status: :active, kind: :manager, email: 'jane.doe@example.com', password: 'password'
 
 Product.create name: 'Smartphone', description: 'Coolest phone ever', status: :active
 Product.create name: 'Tablet', description: 'Coolest tablet!', status: :active
