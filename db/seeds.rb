8b0989711d012f22bc94c7b4f324387cf91b10dc
Mauricio Vieira <mauricio@mauriciovieira.net>
2018-06-28 23:56:04 +0300

    Seed data with upsert

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
-User.create name: 'John Smith', status: :active, kind: :salesperson, email: 'john.smith@example.com', password: 'password'
-User.create name: 'John Doe', status: :active, kind: :salesperson, email: 'john.doe@example.com', password: 'password'
-User.create name: 'Jane Doe', status: :active, kind: :manager, email: 'jane.doe@example.com', password: 'password'
+User.upsert name: 'John Smith', status: :active, kind: :salesperson, email: 'john.smith@example.com', password: 'password'
+User.upsert name: 'John Doe', status: :active, kind: :salesperson, email: 'john.doe@example.com', password: 'password'
+User.upsert name: 'Jane Doe', status: :active, kind: :manager, email: 'jane.doe@example.com', password: 'password'
 
-Product.create name: 'Smartphone', description: 'Coolest phone ever', status: :active
-Product.create name: 'Tablet', description: 'Coolest tablet!', status: :active
+Product.upsert sku: 'SKU-001', name: 'Smartphone', description: 'Coolest phone ever', status: :active
+Product.upsert sku: 'SKU-002', name: 'Tablet', description: 'Coolest tablet!', status: :active
 
-Discount.create name: 'Thanksgiving', description: 'Thanksgiving, black friday discount!', value: '10', kind: :percentage, status: :active
+Discount.upsert coupon: 'TSK-01', name: 'Thanksgiving', description: 'Thanksgiving, black friday discount!', value: '10', kind: :percentage, status: :active
