a0aad8f2a411b3aa96f4c27a8ac1ff74b039a47d
Mauricio Vieira <mauricio@mauriciovieira.net>
2018-06-29 22:07:53 +0300

    Add missing migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_28_204920) do
+ActiveRecord::Schema.define(version: 2018_06_28_205425) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -84,6 +84,8 @@ ActiveRecord::Schema.define(version: 2018_06_28_204920) do
     t.integer "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "coupon"
+    t.index ["coupon"], name: "index_discounts_on_coupon", unique: true
   end
 
   create_table "products", force: :cascade do |t|
