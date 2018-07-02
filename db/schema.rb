b16bd7a2b4b53e1dc843c35d1888ffbf696da139
Mauricio Vieira <mauricio@mauriciovieira.net>
2018-07-02 23:23:02 +0300

    Add price to products table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_02_202033) do
+ActiveRecord::Schema.define(version: 2018_07_02_202240) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -95,6 +95,7 @@ ActiveRecord::Schema.define(version: 2018_07_02_202033) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "sku"
+    t.decimal "price"
     t.index ["sku"], name: "index_products_on_sku", unique: true
   end
 
