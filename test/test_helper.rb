3957091a8abfff123eda93c9fe29d4ab573a7fd7
Mauricio Vieira <mauricio@mauriciovieira.net>
2018-07-22 22:27:40 +0300

    Add smoke test for dashboard

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,5 +6,6 @@ class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
+  include Warden::Test::Helpers
   # Add more helper methods to be used by all tests here...
 end
