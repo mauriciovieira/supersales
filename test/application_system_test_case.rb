981aebbd6cdd9b033a8f76ca1a788aabef0a2603
Mauricio Vieira <mauricio@mauriciovieira.net>
2018-07-26 20:49:56 +0300

    Do not try to get server

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -4,20 +4,19 @@ class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
 
   def self.override_driven_by
     if ENV['SELENIUM_REMOTE_HOST']
-      Capybara.javascript_driver = :selenium_remote_firefox
-      Capybara.register_driver "selenium_remote_firefox".to_sym do |app|
-        Capybara::Selenium::Driver.new(
-          app,
-          browser: :remote,
-          url: "http://#{ENV['SELENIUM_REMOTE_HOST']}:4444/wd/hub",
-          desired_capabilities: :chrome)
-      end
+      # Docker environment
+      driven_by :selenium, using: :chrome, options: {
+        browser: :remote,
+        url: "http://#{ENV['SELENIUM_REMOTE_HOST']}:4444/wd/hub",
+        desired_capabilities: :chrome
+      }
 
-      # Find Docker IP address
-      docker_ip = `/sbin/ip route|awk '/scope/ { print $7 }'`.strip
-      Capybara.server_host = docker_ip
-      Capybara.server_port = '3000'
-      Capybara.app_host = "http://#{Capybara.current_session.server.host}:#{Capybara.current_session.server.port}"
+      # # Find Docker IP address
+      # docker_ip = `/sbin/ip route|awk '/scope/ { print $7 }'`.strip
+      # Capybara.server_host = docker_ip
+      # Capybara.server_port = '3000'
+      # debugger
+      # Capybara.app_host = "http://#{Capybara.current_session.server.host}:#{Capybara.current_session.server.port}"
     else
       # Local environment
       driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
