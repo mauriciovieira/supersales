5799c91d45f857a95e55171dfa553288ab903a4d
Mauricio Vieira <mauricio@mauriciovieira.net>
2018-07-26 21:01:27 +0300

    Clean up

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -10,13 +10,6 @@ class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
         url: "http://#{ENV['SELENIUM_REMOTE_HOST']}:4444/wd/hub",
         desired_capabilities: :chrome
       }
-
-      # # Find Docker IP address
-      # docker_ip = `/sbin/ip route|awk '/scope/ { print $7 }'`.strip
-      # Capybara.server_host = docker_ip
-      # Capybara.server_port = '3000'
-      # debugger
-      # Capybara.app_host = "http://#{Capybara.current_session.server.host}:#{Capybara.current_session.server.port}"
     else
       # Local environment
       driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
