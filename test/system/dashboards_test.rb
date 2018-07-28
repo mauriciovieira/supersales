b0cec35a9c12b820c0e47de89435a9c297ae6e11
Mauricio Vieira <mauricio@mauriciovieira.net>
2018-07-28 23:28:43 +0300

    Test selectors for all models

--- test/system/dashboards_test.rb
+++ test/system/dashboards_test.rb
@@ -5,5 +5,14 @@ class DashboardsTest < ApplicationSystemTestCase
     login_as(users(:jane_doe))
     visit rails_admin.dashboard_path
     assert_current_path rails_admin.dashboard_path
+
+    assert_selector "[data-model=address] a", text: "Addresses"
+    assert_selector "[data-model=client] a", text: "Clients"
+    assert_selector "[data-model=commission] a", text: "Commission"
+    assert_selector "[data-model=discount] a", text: "Discounts"
+    assert_selector "[data-model=product] a", text: "Products"
+    assert_selector "[data-model=sale] a", text: "Sales"
+    assert_selector "[data-model=sales_item] a", text: "Sales items"
+    assert_selector "[data-model=user] a", text: "Users"
   end
 end
